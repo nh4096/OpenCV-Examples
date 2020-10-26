4f64f4c31f09ef24c17e60099bf122d77f1a5b35
neil <nh4096@hotmail.com>
2020-10-26 14:33:11 +0800

    add edge detection

--- Demo1/Demo1.cpp
+++ Demo1/Demo1.cpp
@@ -1,30 +1,52 @@
 ﻿#include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <iostream>
 
 using namespace cv;
 using namespace std;
 
+void usage(const string& prog)
+{
+    cout
+        << "----------------------------------------------------------------" << endl
+        << " Usage: " << prog << " <in_image> <out_image>" << endl
+        << "         in_image: Input image file" << endl
+        << "         out_image: Output image file" << endl
+        << "----------------------------------------------------------------" << endl;
+}
+
 int main(int argc, char** argv)
 {
-    if (argc != 2)
+    if (argc != 3)
     {
-        cout << " Usage: " << argv[0] << " ImageToLoadAndDisplay" << endl;
+        usage(argv[0]);
+        return -1;
+    }
+    
+    cout << "Loading image" << endl;
+    Mat input = imread(argv[1], IMREAD_COLOR); // Read the file
+    if (input.empty()) // Check for invalid input
+    {
+        cout << "Could not open the image" << endl;
         return -1;
     }
 
-    Mat image;
-    image = imread(argv[1], IMREAD_COLOR); // Read the file
-    if (image.empty()) // Check for invalid input
-    {
-        cout << "Could not open or find the image" << std::endl;
-        return -1;
-    }
+    // cout << "shape (w,h): " << input.cols << "," << input.rows << endl;
 
-    cout << "Shape (width,height): " << image.cols << "," << image.rows << endl;
     namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
-    imshow("Display window", image); // Show our image inside it.
+    imshow("Display window", input); // Show our image inside it.
     waitKey(0); // Wait for a keystroke in the window
+
+    cout << "Detecting edges" << endl;
+    Mat edges;
+    Canny(input, edges, 10, 100, 3, false);
+
+    imwrite(argv[2], edges);
+    namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
+    imshow("Display window", edges); // Show our image inside it.
+    waitKey(0); // Wait for a keystroke in the window
+
     return 0;
 }
